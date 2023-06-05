bc8c37d0bec6ff83c0bd94ea7a57e5e73e93550c
hoangluong202 <hl01012002@gmail.com>
2023-06-06 02:33:59 +0700

    comment for understand about middleware bcrypt jwt(not done)

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,33 +1,51 @@
 import User from "../models/User.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
 
-// dotenv.config({ path: "../.env" });
-
+/**
+ * Register function to registry account(email,username,password)
+ * Hash password and save it in to DB
+ * Catch error or response User has been created
+ */
 export const register = async (req, res, next) => {
   try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+    /**Hashing password */
+    const salt = bcrypt.genSaltSync(10); //create random salt value with round is 10
+    const hash = bcrypt.hashSync(req.body.password, salt); //hash password with key is salt value
 
+    /**Create User Models from request */
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hash,
+      password: hash, //password has been hash
     });
+
+    /**Save information of new User(username,password(hashed),email) on mongoDB */
     await newUser.save();
+
+    /**Response status and send inf */
     res.status(200).send("User has been created");
   } catch (err) {
+    /**Catch err and goto middlewares process err in index.js */
     next(err);
   }
 };
 
+/**
+ * Login function: check username and password to login
+ */
 export const login = async (req, res, next) => {
   try {
+    /**In database User, find one user with username is from request and save in user
+     * If not find, use middlewares error handler to response(index.js)
+     */
     const user = await User.findOne({ username: req.body.username });
     if (!user) return next(createError(404, "User not found"));
 
+    /**Use bcrypt lib to compare(plain text, hash)
+     * If diff -> middleware error handler in index.js
+     */
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password
@@ -35,16 +53,22 @@ export const login = async (req, res, next) => {
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username"));
 
+    /**
+     * Generate token with payload(id and isAdmin) + secret: JWT ENV
+     * Sign with default alg: SHA256
+     */
     const token = jwt.sign(
       { id: user.id, isAdmin: user.isAdmin },
       process.env.JWT
     );
 
+    /**
+     * tech to send json without isAdmin and password
+     */
     const { isAdmin, password, ...otherDetails } = user._doc;
+
     res
-      .cookie("access-token", token, {
-        httpOnly: true,
-      })
+      .cookie("access-token", token, { httpOnly: true })
       .status(200)
       .json({ ...otherDetails });
   } catch (err) {
--- api/index.js
+++ api/index.js
@@ -23,13 +23,8 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
 
-//middlewares
+/**Middlewares */
 app.use(cookieParser());
-app.use((req, res, next) => {
-  console.log("hi i am a middleware");
-  next();
-});
-
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
